9994d1e0b38fa0dca9c5b01f157ecd32db3a24e8
Robert05-stack <johnrobertabaya@gmail.com>
2024-10-18 11:10:11 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+let favSingers = ['Bruno Mars','James Arthur', 'Arthur Nery'];
+
+console.log(favSingers[0]); // Output Bruno Mars:  if you enter 1 the output will be James Arthur
+
+let favNums = [11, 20, 24, 99];
+
+let mixedArray = ['string', ['otherarray'], 123, true];
+
+console.log(mixedArray[0]); // string
+console.log(mixedArray[1]); // [ 'otherarray' ]
+console.log(mixedArray[2]); // 123
+console.log(mixedArray[3]); // true
